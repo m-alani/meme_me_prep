09723b04d5c2ef5783fd1ef2d145d8db45a971a0
Marwan Alani <marwan@alani.ca>
2017-02-28 16:47:02 -0500

    Initial Commit

---
+++ MemeMe Prep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe Prep
+//
+//  Created by Marwan Alani on 2017-02-28.
+//  Copyright © 2017 Marwan Alani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
