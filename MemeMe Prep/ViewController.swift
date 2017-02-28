4f98b0bf9f88baaa32dd3f760340fcd5a69540a6
Marwan Alani <marwan@alani.ca>
2017-02-28 17:27:26 -0500

    Picking an image from the camera roll using a UIImagePickerController

--- MemeMe Prep/ViewController.swift
+++ MemeMe Prep/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    @IBAction func pickImage(_ sender: Any) {
+        let pickController = UIImagePickerController()
+        pickController.delegate = self
+        self.present(pickController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
 
